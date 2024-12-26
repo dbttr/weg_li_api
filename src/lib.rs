262b837d9664d366fe8c85004468fff62b6fd4e4
dbttr <dbttr@proton.me>
2024-12-26 08:18:31 +0100

    publish types in lib

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,9 @@ pub mod api;
 pub mod types;
 
 pub use api::WegLiApiClient;
+pub use types::charge::{Charge, ChargeJson};
+pub use types::district::{District, DistrictJson};
+pub use types::export::{
+    Export, ExportDownload, ExportJson, ExportNotice, ExportNoticeCsv, ExportType,
+};
+pub use types::notice::{Notice, NoticeJson, NoticePhotosJson, NoticeStatus};
